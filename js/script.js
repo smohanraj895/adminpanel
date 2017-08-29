87301a52eb66486ebfbd5d5ad4ba7409233c8fb6
smohanraj895 <smohanraj895@gmail.com>
2017-08-29 14:25:49 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 
+$(document).ready(function(){
+    $("#btn1").click(function(){
+        $("#test1").text("Hello world!");
+    });
+    $("#btn2").click(function(){
+        $("#test2").html("<b>Hello world!</b>");
+    });
+    $("#btn3").click(function(){
+        $("#test3").val("Dolly Duck");
+    });
+});
